56cb404fd04b232d81f1bf2f4c69c12d9f2b4672
startupowe1konto1 <startupaplikujmy@gmail.com>
2026-03-17 21:10:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { WaitlistProvider } from "@/context/WaitlistContext";
 import Index from "./pages/Index";
 import SampleReport from "./pages/SampleReport";
+import PolitykaPrywatnosci from "./pages/PolitykaPrywatnosci";
+import PolitykaCookies from "./pages/PolitykaCookies";
+import Regulamin from "./pages/Regulamin";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +23,9 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/sample-report" element={<SampleReport />} />
+            <Route path="/polityka-prywatnosci" element={<PolitykaPrywatnosci />} />
+            <Route path="/polityka-cookies" element={<PolitykaCookies />} />
+            <Route path="/regulamin" element={<Regulamin />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
